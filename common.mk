eb866c4ba51d4e197b7c21f246547622fb56518e
mj1687 <alessandro.pasini12@gmail.com>
2018-07-08 14:49:16 +0200

    initialize treble.
    i'm trying to initialize the tree for treble by looking at how lineage team have done this on the essential phone
    
    Signed-off-by: mj1687 <alessandro.pasini12@gmail.com>

--- common.mk
+++ common.mk
@@ -468,9 +468,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/wifi_concurrency_cfg.txt:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wifi_concurrency_cfg.txt
 
-# Inherit vendor
-$(call inherit-product, vendor/oneplus/msm8998-common/msm8998-common-vendor.mk)
-
 # Inherit from oppo-common
 $(call inherit-product, device/oppo/common/common.mk)
 
