81a2ec9fb927cb35825c8ffd77d29893c37e3e0b
Salvatore Festa <salvatorefestatech@gmail.com>
2018-07-29 10:46:23 +0200

    Don't build SnapdragonCamera

--- common.mk
+++ common.mk
@@ -151,7 +151,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service.oneplus5
-    SnapdragonCamera
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
