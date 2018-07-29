6d0d400c36ccc04e48b30d36be6d37dc527fe267
Salvatore Festa <salvatorefestatech@gmail.com>
2018-07-29 14:48:19 +0200

    Build Snap

--- common.mk
+++ common.mk
@@ -150,7 +150,8 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service.oneplus5
+    android.hardware.camera.provider@2.4-service.oneplus5 \
+    Snap
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
